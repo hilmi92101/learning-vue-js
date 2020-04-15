5101e3ab6a0d4b01d3853ccd59de4a0fc2f9326f
Hilmi <muhammadhilmi92101@gmail.com>
2020-04-15 19:53:45 +0800

    Conditional: if, show

--- app.js
+++ app.js
@@ -3,29 +3,14 @@ new Vue({
 	el:'#vue-app',
 
 	data: {
-        available: false,
-        borderCard: false,
-        nearby: false
+        error: false,
+        success: false
     },
-
     methods: {
 
-    	classChange(){
-
-    		this.available = !this.available;
-    		this.borderCard = !this.borderCard;
-    	}
-
     },
-
     computed: {
 
-        compClasses: function(){
-            return{
-                available: this.available,
-                nearby: this.nearby,
-            }
-        }
     }
 
 });
