9eb4488bfe78aca69d836d90a0343ef69afc966a
Hilmi <muhammadhilmi92101@gmail.com>
2020-04-15 17:42:21 +0800

    Multiple CSS Classes

--- app.js
+++ app.js
@@ -4,15 +4,22 @@ new Vue({
 
 	data: {
         available: false,
+        borderCard: false,
         nearby: false
     },
 
     methods: {
 
+    	classChange(){
+
+    		this.available = !this.available;
+    		this.borderCard = !this.borderCard;
+    	}
+
     },
 
     computed: {
-    	
+
         compClasses: function(){
             return{
                 available: this.available,
