33ba40128c591a61baefaad11da349eb7aaebb12
Hilmi <muhammadhilmi92101@gmail.com>
2020-04-15 17:28:11 +0800

    Dynamic CSS Classes

--- app.js
+++ app.js
@@ -3,33 +3,21 @@ new Vue({
 	el:'#vue-app',
 
 	data: {
-
-
-		a: 0,
-        b: 0,
-        age: 20
-		
-
-	},
-
-	methods: {
-        /*addToA: function(){
-            console.log('addToA');
-            return this.a + this.age;
-        },
-        addToB: function(){
-            console.log('addToB');
-            return this.b + this.age;
-        }*/
+        available: false,
+        nearby: false
     },
+
+    methods: {
+
+    },
+
     computed: {
-        addToA: function(){
-            console.log('addToA');
-            return this.a + this.age;
-        },
-        addToB: function(){
-            console.log('addToB');
-            return this.b + this.age;
+    	
+        compClasses: function(){
+            return{
+                available: this.available,
+                nearby: this.nearby,
+            }
         }
     }
 
