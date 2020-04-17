6070aaa495784a85b5e92911e35941ec43e11c19
Hilmi <muhammadhilmi92101@gmail.com>
2020-04-17 15:31:35 +0800

    Refs

--- app.js
+++ app.js
@@ -1,58 +1,14 @@
-
-Vue.component('greeting', {
-
-    template: `
-    	<div>
-	    	<p style="color:red;">Hey there, I am {{ name }}</p>
-	    	<button @click="changeName">Change name</button>
-    	</div>
-
-
-    `,
-   
-    data: function(){
-
-    	return {
-
-    		name: 'Yoshi'
-
-    	}
-    },
-
-    methods: {
-
-    	changeName: function(){
-
-    		this.name = 'Mario';
-    	}
-
-    }
-
-
-
-});
-
-var one = new Vue({
+new Vue({
     el: '#vue-app-one',
     data: {
-    },
-    computed: {
-      
-    },
-    methods: {
-      
-    }
-});
-
-var two = new Vue({
-    el: '#vue-app-two',
-    data: {
-    },
-    computed: {
-      
+        output: 'Your favourite food',
+        output_2: 'Your favourite food'
     },
     methods: {
-     
+        readRefs: function(){
+            console.log(this.$refs);
+            this.output = this.$refs.input.value;
+            this.output_2 = this.$refs.input_2.outerHTML;
+        }
     }
 });
-
